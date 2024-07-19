5fb1e2f207c078e6ce996d0932ce68ef73ba7e9f
rlatosky <latoskyr@gmail.com>
2024-07-19 13:38:02 -0400

    Restructure code. Fix issue where scancel wasn't cancelling jobs, and error handle a keyboard interrupt.

--- SlurmRunner.py
+++ SlurmRunner.py
@@ -10,102 +10,132 @@ import io
 
 parser = argparse.ArgumentParser(description='SLURM Runner written in Python for PyMOO.')
 parser.add_argument("-s", "--script_path", help="Specify script path to be used for sbatch.")
-parser.add_argument("-t", "--refresh", help="Specify how much time we refresh the SLURM job list.", default=2)
-parser.add_argument("-i", "--status", help="Show output of job status.", default=True)
+parser.add_argument("-t", "--refresh", help="Specify how much time we refresh the SLURM job list.", default=1)
 parser.add_argument("-o", "--output_dir", help="Output directory.", default="./")
 parser.add_argument("-r", "--run", help="Run SLURM job.", default=True)
 parser.add_argument("-j", "--jobs", help="Run multiple SLURM jobs.", default=1)
 parser.add_argument("-wt", "--wall_time", help="Force running jobs to stop at a specified time formatted in HH:MM:SS.", default="")
 
 args = parser.parse_args()
-script_path = os.path.abspath(args.script_path)
 
-try:
-    with open(os.path.abspath(args.script_path), "rt") as slurm_file:
-        lines = slurm_file.readlines()
-        for line in lines:
-            if "#SBATCH --job-name=" in line:
-                job_name = line[ line.rfind('/') +1 : line.rfind('#') ].strip()
-            elif "#SBATCH --output=" in line:
-                output_file = line[ line.find('=') +1 : line.rfind('#') ].strip()
-            elif "#SBATCH --error=" in line:
-                error_file = line[ line.find('=') +1 : line.rfind('#') ].strip()
-            elif "#SBATCH --time=" in line:
-                time_limit = line[ line.find('=') +1 : line.rfind('#') ].strip()
-except TypeError:
-    print(f"File with path {os.path.abspath(args.script_path)} not found!")
-    exit()
+def read_slurm_script():
+    try:
+        with open(os.path.abspath(args.script_path), "rt") as slurm_file:
+            lines = slurm_file.readlines()
+            for line in lines:
+                if "#SBATCH --job-name=" in line:
+                    job_name = line[ line.rfind('/') +1 : line.rfind('#') ].strip()
+                elif "#SBATCH --output=" in line:
+                    output_file = line[ line.find('=') +1 : line.rfind('#') ].strip()
+                elif "#SBATCH --error=" in line:
+                    error_file = line[ line.find('=') +1 : line.rfind('#') ].strip()
+                elif "#SBATCH --time=" in line:
+                    time_limit = line[ line.find('=') +1 : line.rfind('#') ].strip()
+        return job_name, output_file, error_file, time_limit
+    except TypeError:
+        print(f"File with path {os.path.abspath(args.script_path)} not found!")
+        exit()
 
-print(f"--> Using SLURM script: {os.path.abspath(args.script_path)}")
-print(f"--> Output directory set to: {os.path.abspath(args.output_dir)}")
-os.makedirs(os.path.abspath(args.output_dir),exist_ok=True)
-print(f"--> Output file: {os.path.abspath(args.output_dir)}/{output_file}\n")
-time.sleep(3)
+def print_paths_and_make_output_dir():
+    print(f"--> Using SLURM script: {os.path.abspath(args.script_path)}")
+    print(f"--> Output directory set to: {os.path.abspath(args.output_dir)}")
+    os.makedirs(os.path.abspath(args.output_dir),exist_ok=True)
 
-def run_jobs(num_of_jobs=int(args.jobs), show_status=args.status):
-    job_id_list = []
+def create_folder_and_change_dir_for_job(job):
+    path = os.path.abspath(args.output_dir) + "/ITER_" + str(job)
+    os.makedirs(path, exist_ok=True)
+    os.chdir(path)
+
+def convert_time_limit_to_sec(time_limit):
+    if args.wall_time:
+        hh, mm, ss = args.wall_time.split(':')
+    else:
+        hh, mm, ss = time_limit.split(':')
+    time_limit_int = int(hh) * 3600 + int(mm) * 60 + int(ss)
+    return time_limit_int
+
+def create_jobs(num_of_jobs=int(args.jobs)):
     cwd = os.getcwd()
+    job_id_list = []
     for job in range(num_of_jobs):
-        path = os.path.abspath(args.output_dir) + "/ITER_" + str(job)
-        os.makedirs(path, exist_ok=True)
-        os.chdir(path)
-        print(f"sbatch {os.path.abspath(script_path)}")
-        result = subprocess.run(["sbatch", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
+        create_folder_and_change_dir_for_job(job)
+        cmd = f"sbatch {args.script_path}"
+        result = subprocess.run(["sbatch", f"{args.script_path}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         try:
             job_id = re.findall(r'\d+', result.stdout)[0]
             job_id_list.append(job_id)
         except IndexError:
             print("Cannot find job ID!")
-        os.chdir(cwd)
-            
-    start_time = time.time()
-    if args.wall_time:
-        time_limit_val = args.wall_time
-    else:
-        time_limit_val = time_limit
-    hh, mm, ss = time_limit_val.split(':')
-    time_limit_int = int(hh) * 3600 + int(mm) * 60 + int(ss)
+        os.chdir(cwd) # Restore working directory where we ran the script
+    return job_id_list
 
+def poll_jobs(time_limit, job_id_list):
+    time_limit_int = convert_time_limit_to_sec(time_limit)
     try:
-        with subprocess.run(["watch","-c","-d","-e","-n", str(args.refresh), "squeue", "--job", f"{','.join(map(str, job_id_list))}", " | grep $USER"], stdin=subprocess.PIPE, stdout=sys.stdout, stderr=sys.stderr, timeout=time_limit_int) as proc:
+        with subprocess.run(
+                ["watch", "-c", "-d", "-e", "-n", f"{str(args.refresh)}", "squeue", "--job", f"{','.join(map(str, job_id_list))}", "| grep $USER"],
+                stdin=subprocess.PIPE, 
+                stdout=sys.stdout, 
+                stderr=sys.stderr, 
+                timeout=time_limit_int
+            ) as proc:
             try:
                 sys.stdout.write(proc.stdout)
             except TypeError:
                 time.sleep(1)
-    except AttributeError:
+    except AttributeError:  # Even though this is an error, this will tell us when there are no more jobs for $USER
         print(f"Jobs Completed\n")
     except subprocess.TimeoutExpired:
         sys.stdout.flush()
         time.sleep(5)
-        #os.system('clear')
-        os.system('reset')
+        os.system('reset')  # We have to reset otherwise frame buffer is contaminated
         print(f"Current running time has exceeded wall-time limit for jobs!")
         print(f"Cancelling SLURM jobs!")
+        kill_jobs(job_id_list)
         time.sleep(1)
-        try:
-            check_output = subprocess.check_output(f"squeue --job {','.join(map(str, job_id_list)) } | grep $USER", shell=True, universal_newlines=True)
-            time.sleep(2)
-            check_output_list = list(filter(None, check_output.split('\n')))
-            failed_job_id_list = []
-            for line in check_output_list:
-                try:
-                    failed_job_id = re.search(r'\d+', line).group()
-                    failed_job_id_list.append(failed_job_id)
-                    print(f"CANCELLED JOB WITH ID {failed_job_id}")
-                except AttributeError:
-                    print(f"Cannot find job ID!")
-            subprocess.check_output(f"scancel --job {','.join(map(str, failed_job_id_list)) } | grep $USER", shell=True, universal_newlines=True)
-            #os.system('clear')
-        except subprocess.CalledProcessError:
-            print(f"Jobs finished or killed\n")
-            time.sleep(10)
 
+def kill_jobs(job_id_list):
+    try:
+        check_output = subprocess.check_output(f"squeue --job {','.join(map(str, job_id_list)) } | grep $USER", shell=True, universal_newlines=True)
+        check_output_list = list(filter(None, check_output.split('\n')))
+        failed_job_id_list = []
+        for line in check_output_list:
+            try:
+                failed_job_id = re.search(r'\d+', line).group()
+                failed_job_id_list.append(failed_job_id)
+                print(f"CANCELLED JOB WITH ID {failed_job_id}")
+            except AttributeError:
+                print(f"Cannot find job ID!")
+        #subprocess.run(f"scancel --job {','.join(map(str, failed_job_id_list)) }", shell=True, universal_newlines=True)
+        subprocess.run(
+            ["scancel", f"{','.join(map(str, failed_job_id_list))}"], 
+            stdin=subprocess.PIPE, 
+            stdout=sys.stdout, 
+            stderr=sys.stderr, 
+            universal_newlines=True
+                       )
+    except subprocess.CalledProcessError:
+        print(f"Jobs finished or killed\n")
+        time.sleep(10)
+
+def print_job_output(num_of_jobs=int(args.jobs)):
     print(f"===OUTPUT OF JOBS===\n")
     print("Output is available under these directories:")
     for job in range(num_of_jobs):
         print(os.path.abspath(args.output_dir) + "/ITER_" + str(job))
-
     print(f"\n===END OF OUTPUT FOR JOBS===\n")
 
+def run_jobs(num_of_jobs=int(args.jobs)):
+    try:
+        job_name, output_file, error_file, time_limit = read_slurm_script()
+        print_paths_and_make_output_dir()
+
+        job_id_list = create_jobs()
+        poll_jobs(time_limit, job_id_list)
+
+        print_job_output()
+    except KeyboardInterrupt:
+        kill_jobs(job_id_list)
+
 if __name__ == "__main__":
-        run_jobs()
+    run_jobs()
